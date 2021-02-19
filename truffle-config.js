72d0dc3a9f1e0a291cc219370e9252eef45badaa
Don <dev@solida.io>
2021-02-18 19:01:50 -0700

    deploy

--- truffle-config.js
+++ truffle-config.js
@@ -8,6 +8,17 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          privateKeys.split(','), // Array of account private keys
+          `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}` // URL to an Ethereum node
+          )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
